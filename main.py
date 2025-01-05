750a55ae637130c19c46294de53064b118985606
Sudeep3000 <Sudeep3000@users.noreply.github.com>
2025-01-05 17:50:23 +0530

    changes

--- main.py
+++ main.py
@@ -234,6 +234,5 @@ def deleteBook(bookid):
 
 
 eel.init("gui")
-myconn = dbConnect("localhost","root","root")
-if myconn != None:
-    eel.start("login.html")
+#myconn = dbConnect("localhost","root","root")
+eel.start("login.html")
